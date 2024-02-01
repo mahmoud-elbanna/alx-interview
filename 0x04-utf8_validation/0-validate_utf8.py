e39d4522754fc084b01bfac4b5703507d7340fa9
Mah-moud Elbanna <125469051+mahmoud-elbanna@users.noreply.github.com>
2024-02-01 22:45:24 +0200

    Create 0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+0-validate_utf8 module
+"""
+
+
+def validUTF8(data) -> bool:
+    """Determines if a given data set represents a valid UTF-8 encoding"""
+
+    def byte_sequence_count(byte):
+        """Returns the number of bytes in a UTF-8 sequence"""
+        binary_representation = bin(byte)[2:].rjust(8, "0")
+        if binary_representation.startswith("0"):
+            return 1
+        elif binary_representation.startswith("110"):
+            return 2
+        elif binary_representation.startswith("1110"):
+            return 3
+        elif binary_representation.startswith("11110"):
+            return 4
+        else:
+            return -1
+
+    i = 0
+
+    while i < len(data):
+        sequence_count = byte_sequence_count(data[i])
+        if sequence_count == -1:
+            return False
+
+        i += 1
+        if sequence_count == 1:
+            continue
+
+        if i + sequence_count - 1 > len(data):
+            return False
+
+        for j in range(sequence_count - 1):
+            if not (data[i + j] & 0b11000000 == 0b10000000):
+                return False
+
+        i += sequence_count - 1
+
+    return True
